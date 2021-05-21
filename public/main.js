b427745dd7618aad725a68d70b96316d71851235
Nathan Phame <diyf2021@gmail.com>
2021-05-21 13:33:36 -0400

    Added Spock and Lizard to game

--- public/main.js
+++ public/main.js
@@ -16,7 +16,7 @@ document
 function updateSelectionPaperOne(event) {
   const selectionOne = document.querySelector('.choiceOne')
   selectionOne.textContent = 'paper'
-  playerChoiceOne = paper
+  playerChoiceOne = 'paper'
 }
 
 document
@@ -32,6 +32,26 @@ function updateSelectionScissorsOne(event) {
 document
   .querySelector('.scissorsOne')
   .addEventListener('click', updateSelectionScissorsOne)
+
+function updateSelectionSpockOne(event) {
+  const selectionOne = document.querySelector('.choiceOne')
+  selectionOne.textContent = 'spock'
+  playerChoiceOne = 'spock'
+}
+
+document
+  .querySelector('.spockOne')
+  .addEventListener('click', updateSelectionSpockOne)
+
+function updateSelectionLizardOne(event) {
+  const selectionOne = document.querySelector('.choiceOne')
+  selectionOne.textContent = 'lizard'
+  playerChoiceOne = 'lizard'
+}
+
+document
+  .querySelector('.lizardOne')
+  .addEventListener('click', updateSelectionLizardOne)
 
 function updateSelectionRockTwo(event) {
   const selectionTwo = document.querySelector('.choiceTwo')
@@ -63,20 +83,42 @@ document
   .querySelector('.scissorsTwo')
   .addEventListener('click', updateSelectionScissorsTwo)
 
+function updateSelectionSpockTwo(event) {
+  const selectionTwo = document.querySelector('.choiceTwo')
+  selectionTwo.textContent = 'spock'
+  playerChoiceTwo = 'spock'
+}
+
+document
+  .querySelector('.spockTwo')
+  .addEventListener('click', updateSelectionSpockTwo)
+
+function updateSelectionLizardTwo(event) {
+  const selectionTwo = document.querySelector('.choiceTwo')
+  selectionTwo.textContent = 'lizard'
+  playerChoiceTwo = 'lizard'
+}
+
+document
+  .querySelector('.lizardTwo')
+  .addEventListener('click', updateSelectionLizardTwo)
 function final(event) {
   if (playerChoiceOne === playerChoiceTwo) {
     window.alert('draw')
   } else if (
     (playerChoiceOne === 'rock' && playerChoiceTwo === 'scissors') ||
     (playerChoiceOne === 'paper' && playerChoiceTwo === 'rock') ||
-    (playerChoiceOne === 'scissors' && playerChoiceTwo === 'paper')
+    (playerChoiceOne === 'scissors' && playerChoiceTwo === 'paper') ||
+    (playerChoiceOne === 'rock' && playerChoiceTwo === 'lizard') ||
+    (playerChoiceOne === 'scissors' && playerChoiceTwo === 'lizard') ||
+    (playerChoiceOne === 'paper' && playerChoiceTwo === 'spock') ||
+    (playerChoiceOne === 'spock' && playerChoiceTwo === 'scissors') ||
+    (playerChoiceOne === 'spock' && playerChoiceTwo === 'rock') ||
+    (playerChoiceOne === 'lizard' && playerChoiceTwo === 'spock') ||
+    (playerChoiceOne === 'lizard' && playerChoiceTwo === 'paper')
   ) {
     window.alert('player one won')
-  } else if (
-    (playerChoiceTwo === 'rock' && playerChoiceOne === 'scissors') ||
-    (playerChoiceTwo === 'paper' && playerChoiceOne === 'rock') ||
-    (playerChoiceTwo === 'scissors' && playerChoiceOne === 'paper')
-  ) {
+  } else {
     window.alert('player two won')
   }
 }
